66821a05783804964d1b9c25a39d2078ca3344e8
Sergey A <sergalf81@mail.ru>
2023-10-20 11:37:07 +0300

    Добавлена програма к ДЗ1

---
+++ C#_HW_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Dynamic;
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+NaturalFromMaxToMin(number, count);
+
+void NaturalFromMaxToMin(int n, int count)
+{
+    if(count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalFromMaxToMin(n, count + 1);
+        System.Console.Write(count + ", ");
+    }
+}
+System.Console.WriteLine();
