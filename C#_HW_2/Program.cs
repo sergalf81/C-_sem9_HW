18353857e27f55e9e57973fbfb5125d175852109
Sergey A <sergalf81@mail.ru>
2023-10-21 10:38:10 +0300

    Коррекция ДЗ2

--- C#_HW_2/Program.cs
+++ C#_HW_2/Program.cs
@@ -6,13 +6,11 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 SumFromMToN(m, n);
 
-// вызов функции "сумма чисел от M до N"
 void SumFromMToN(int m, int n)
 {
     Console.Write(SumMN(m - 1, n));
 }
 
-// функция сумма чисел от M до N
 int SumMN(int m, int n)
 {
     int res = m;
